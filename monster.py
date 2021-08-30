2b65f32e7939a7452bc0c04570351fcd4d782fd4
smoke boy <86958703+suhaib187016@users.noreply.github.com>
2021-08-30 14:42:49 +0300

    Create monster.py

---
+++ monster.py
@@ -0,0 +1,18 @@
+import os,sys,time
+os.sysrem("clear")
+def nop
+
+print("suhaib hello !!!!)
+
+nop()
+
+time.sleep("10")
+def nop()
+
+    print("installl 111111")
+
+nop()
+
+time.sleep(15)
+
+os.system("pkg install git")
